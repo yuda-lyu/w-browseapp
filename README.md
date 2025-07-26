25e564ab3fc5ee8def31098e7c536751029c36c1
yuda-lyu <firsemisphere@gmail.com>
2025-07-26 21:03:07 +0800

    first upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# w-browseapp
+A warpper app using settings by electron. 
+
+![language](https://img.shields.io/badge/language-JavaScript-orange.svg) 
+[![npm version](http://img.shields.io/npm/v/w-browseapp.svg?style=flat)](https://npmjs.org/package/w-browseapp) 
+[![license](https://img.shields.io/npm/l/w-browseapp.svg?style=flat)](https://npmjs.org/package/w-browseapp) 
+[![npm download](https://img.shields.io/npm/dt/w-browseapp.svg)](https://npmjs.org/package/w-browseapp) 
+[![npm download](https://img.shields.io/npm/dm/w-browseapp.svg)](https://npmjs.org/package/w-browseapp) 
+[![jsdelivr download](https://img.shields.io/jsdelivr/npm/hm/w-browseapp.svg)](https://www.jsdelivr.com/package/npm/w-browseapp)
+
+## run
+```
+npm run start
+```
+
+## build(須使用系統管理員之權限才能編譯)
+```
+npm run build
+```
---
+++ main.js
@@ -0,0 +1,74 @@
+const path = require('node:path');
+const fs = require('node:fs');
+const { app, BrowserWindow } = require('electron');
+
+//fdExe
+let fdExe = process.env.PORTABLE_EXECUTABLE_DIR || __dirname
+
+//fpSettings
+let fpSettings = path.join(fdExe, 'settings.json');
+console.log('fpSettings',fpSettings)
+
+//st
+let st;
+try {
+  st = JSON.parse(fs.readFileSync(fpSettings, 'utf8'));
+} catch (err) {
+  console.error('can not read settings.json', err);
+  process.exit(1);
+}
+console.log('st',st)
+
+// 檢查 st.userDataFolder 是否為有效字串
+if (typeof st.userDataFolder !== 'string' || st.userDataFolder.trim() === '' || st.userDataFolder.trim() === '{your folder}') {
+  console.error(`invalid st.userDataFolder[${st.userDataFolder}]`);
+  process.exit(1);
+}
+
+// 檢查 st.dp 是否為有效數字，若不是則設為 1
+if (typeof st.dp !== 'number' || st.dp === 0) {
+  console.warn(`invalid or missing st.dp[${st.dp}], defaulting to 1.`);
+  st.dp = 1;
+}
+
+app.once('ready', () => {
+
+  // 設定 userData 資料夾
+  if (st.userDataFolder) {
+    app.setPath('userData', st.userDataFolder);
+  }
+
+  // 建立 BrowserWindow
+  const win = new BrowserWindow({
+    x: st.x/st.dp,
+    y: st.y/st.dp,
+    width: st.width/st.dp,
+    height: st.height/st.dp,
+    useContentSize: true,
+    resizable: st.resizable,
+    show: false,
+    webPreferences: {
+      contextIsolation: true,
+      sandbox: false,
+    }
+  });
+
+  // 等網頁載入完成再顯示
+  win.webContents.on('did-finish-load', () => {
+    win.show();
+  });
+
+  // 載入指定 URL
+  win.loadURL(st.url)
+  .then(()=>{
+    win.webContents.setAudioMuted(true);
+  })
+  .catch(err => {
+    console.error('load url error', err);
+  });
+  
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
